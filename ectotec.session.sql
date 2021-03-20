2d0639f6584c2345943dd765debb2c5651b22661
Luis Angulo <luisf117@gmail.com>
2021-03-20 02:49:34 -0700

    Test data sql script for city-state

---
+++ ectotec.session.sql
@@ -0,0 +1,18 @@
+INSERT INTO Ciudades (NombreCiudad, NombreEstado)
+VALUES 
+    ('Cd. Obregón', 'Sonora'),
+    ('Cd. de México', 'Estado de México'),
+    ('Cd. Victoria', 'Tamaulipas'),
+    ('Acaponeta', 'Nayarit'),
+    ('Guadalajara', 'Jalisco'),
+    ('Monterrey', 'Nuevo León'),
+    ('Zamora', 'Michoacán'),
+    ('Celaya', 'Guanajuato'),
+    ('Cuernavaca', 'Morelos'),
+    ('Tepic', 'Nayarit'),
+    ('Campeche', 'Campeche'),
+    ('Chihuahua', 'Chihuahua'),
+    ('Chilpancingo', 'Guerrero'),
+    ('Pachuca', 'Hidalgo'),
+    ('Michoacán', 'Morelia')
+;
